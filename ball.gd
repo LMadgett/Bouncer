9efe606d7d8999e5f4f4c4d6906b5c519b5d18d9
Luke Madgett <luke@madgett.com>
2025-08-29 16:19:10 +0100

    added lives mechanism and reset functionality for the ball

--- ball.gd
+++ ball.gd
@@ -2,23 +2,57 @@ extends AnimatableBody2D
 
 var direction: Vector2
 var speed: float
+@export var bat: AnimatableBody2D
+var start_position
+var timer := Timer.new()
+var start_speed = 650
+var reset = false
+var fixed_to_bat = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	speed = 650
-	direction = Vector2(50.0, 70.0).normalized()
+	add_child(timer)
+	timer.wait_time = 1.0
+	timer.one_shot = true
+	timer.timeout.connect(_on_timer_timeout)
+	speed = start_speed
+	
+	direction = Vector2(randf_range(-0.3, 0.3), 1).normalized()
+	start_position = global_position
+	process_mode = Node.PROCESS_MODE_PAUSABLE
+	
+func _on_timer_timeout():
+	speed = start_speed
+	reset = false
+	fixed_to_bat = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if fixed_to_bat:
+		global_position = Vector2(bat.position.x, bat.position.y - 25)
 
-func _physics_process(delta):
+func _physics_process(delta):	
+	reset = false
 	var collision_info = move_and_collide(speed * direction * delta)
 	while collision_info:
 		var other_object = collision_info.get_collider()
 		if other_object.is_in_group("Brick"):
 			other_object.ball_hit()
-		var movement_remaining := collision_info.get_remainder()
-		var remaining_length = movement_remaining.length()
-		direction = direction.bounce(collision_info.get_normal())
-		collision_info = move_and_collide(direction * remaining_length)
+		elif other_object.is_in_group("ScreenBottom"):
+			bat.die()
+			if bat.health == 0:
+				global_position = start_position
+				speed = 0
+			else:
+				direction = Vector2(0, -1).normalized()
+				collision_info = null
+				reset = true
+				fixed_to_bat = true
+				timer.start()
+				speed = 0
+				global_position = Vector2(bat.position.x, bat.position.y - 25)
+		if not reset:
+			var movement_remaining := collision_info.get_remainder()
+			var remaining_length = movement_remaining.length()
+			direction = direction.bounce(collision_info.get_normal())
+			collision_info = move_and_collide(direction * remaining_length)
--- bat.gd
+++ bat.gd
@@ -2,11 +2,15 @@ extends AnimatableBody2D
 
 var movement: float = 0.0
 @export var movement_scale: float = 75.0
+@export var health: int = 3
+@export var health_label: Label
+var start_position
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-
+	start_position = global_position
+	health_label.text = "Health: " + str(health)
 
 func _input(event):
 	if event.is_action("ui_cancel"):
@@ -24,3 +28,13 @@ func _physics_process(delta):
 	var move: Vector2 = Vector2(movement * delta * movement_scale, 0)
 	var collision_info = move_and_collide(move)
 	movement = 0.0
+
+func die():
+	health -= 1
+	health_label.text = "Health: " + str(health)
+	global_position = start_position
+	if health == 0:
+		print("bat dead")
+		movement_scale = 0
+		global_position = start_position
+		health_label.text = "NO LIVES REMAINING"
